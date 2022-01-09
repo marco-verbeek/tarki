a4196c6b084bec2a37f9312d62b4ae225298d0c1
Marco <marcoverbeek99@gmail.com>
2022-01-09 01:13:44 +0100

    feat(deploy): small change to test gh action

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsModule } from './items/items.module';
+//import { ItemsModule } from './items/items.module';
 import LoggerMiddleware from './middlewares/logger.middleware';
 
 @Module({
@@ -10,7 +10,7 @@ import LoggerMiddleware from './middlewares/logger.middleware';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ItemsModule,
+    //ItemsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
