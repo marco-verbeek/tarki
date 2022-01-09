b11314d5240ccd7d60f30874c48850aad4ba2188
jessica-mankevitch <jessicamankevitch@gmail.com>
2022-01-09 19:43:09 +0100

    add material modules, separate 'price' to 'vendor_price' and 'market_price' & rework style

--- apps/web/src/app/homepage/homepage.component.ts
+++ apps/web/src/app/homepage/homepage.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { HomepageService } from './homepage.service';
+import { ModalComponent } from './modal/modal.component';
+
 
 @Component({
   selector: 'app-homepage',
@@ -6,6 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  @ViewChild(ModalComponent) modal: ModalComponent | undefined;
+
+  newItems: string = "";
+
   headers: string[] = [
     'icon',
     'name',
@@ -15,14 +23,24 @@ export class HomepageComponent {
     'barter_crafting'
   ]
 
-  items: { 'icon': string, 'name': string, 'price': string, 'quest': string, 'hideout': string, 'barter_crafting': string }[] = [{
+  items: { 'icon': string, 'name': string, 'vendor_price': string, 'market_price': string, 'quest': string, 'hideout': string, 'barter_crafting': string }[] = [{
     'icon': '../../assets/img/salewa.jpg',
     'name': 'Salewa first aid kit',
-    'price': '7,427 @ Therapist\n20,762 @ FleaMarket',
+    'vendor_price': '7,427 @ Therapist',
+    'market_price': '20,762 @ FleaMarket',
     'quest': 'Therapist - Shortage 3x',
     'hideout': 'no',
     'barter_crafting': 'no'
   }];
 
-  constructor() { }
+  constructor(
+    private homepageService: HomepageService,
+    public dialog: MatDialog
+  ) { }
+
+  searchItems(): void {
+    this.dialog.open(ModalComponent, {
+      panelClass: "select-items-dialog-container"
+    });
+  }
 }
--- apps/web/src/app/homepage/homepage.module.ts
+++ apps/web/src/app/homepage/homepage.module.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage.component';
+import { ModalComponent } from './modal/modal.component';
+
 
 const routes: Routes = [
   {
@@ -12,10 +17,16 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [
+    HomepageComponent,
+    ModalComponent,
+  ],
   imports: [
     CommonModule,
     MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ]
 })
